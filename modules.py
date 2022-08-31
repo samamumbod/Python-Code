404f8795ddf814de1277d0b8bf2d43c108760232
samamumbod <samamumbod@gmail.com>
2022-08-31 13:40:25 +0100

    Working with modules

---
+++ modules.py
@@ -0,0 +1,19 @@
+# Working with modules
+
+# import math module
+import math
+import os
+os.system("cls")
+
+
+# making use of a function in the math library
+print("The  squre root of of 16 is ", math.sqrt(16))
+
+# print the value of pi
+print("PI is ", math.pi)
+
+
+print("sin(30) = ", math.sin(math.radians(30)) )
+print("sin(60) = ", math.sin(math.radians(60)))
+print("sin(45) = ", math.sin(math.radians(45)))
+
